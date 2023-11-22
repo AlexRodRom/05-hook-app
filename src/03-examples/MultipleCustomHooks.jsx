def9bf26f34c1c1ea3bfad5fba29fed03801befd
Alejandro Rodriguez <alejandro.rodriguez@daappa.com>
2023-11-22 19:10:51 +0000

    Add mezclar dos Hooks

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,8 +1,11 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
 
-    const { data, isLoading, hasError  } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const { counter, operaciones  } = useCounter(1);
+    //const { counter, operaciones } = useCounter( 20 ); // Custom Hook
+    const { data, isLoading, hasError  } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}` );
 
     console.log(data);
     const { author, quote } = !!data && data[0];
@@ -15,6 +18,20 @@ export const MultipleCustomHooks = () => {
     <>
         <h1>BreakingBad Quotes</h1>
         <hr/>
+        <div className="text-start form-control">
+            <button className="btn btn-primary" onClick={ () => operaciones.suma() }>
+                More Quotes 
+            </button>
+            <button className="btn btn-primary" onClick={ () => operaciones.reset() }>
+                Reset Quotes 
+            </button>
+            <button className="btn btn-primary" onClick={ () => operaciones.resta() }>
+                Less Quotes 
+            </button>
+            <p className="mb-3"> Quotes: {!!data && data.length }</p>
+            <p className="mb-3"> Counter: { JSON.stringify(counter) } </p>
+        </div>
+        
         {
             ( isLoading ) 
             ? ( 
@@ -23,14 +40,15 @@ export const MultipleCustomHooks = () => {
                 </div>
             )
             : (
-                <blockquote className="blockquote text-end">
-                    <p className="mb-3">{ quote }</p>
-                    <footer className="blockquote-footer">{ author } </footer>
-                </blockquote>  
+                data.map( (d) => (
+                    <blockquote className="blockquote text-end">
+                        <p className="mb-3">{ d.quote }</p>
+                        <footer className="blockquote-footer">{ d.author } </footer>
+                    </blockquote> 
+                ))
             )
+            
         }
-
-        <button className="btn btn-primary"> Next Quote </button>
 
     </>
   )
