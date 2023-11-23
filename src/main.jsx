4aedcbccc3de899bdc1adab355d4a8584e96787f
Alejandro Rodriguez <alejandro.rodriguez@daappa.com>
2023-11-23 17:36:29 +0000

    Add useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,10 @@ import './index.css'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   // </React.StrictMode>,
  )
