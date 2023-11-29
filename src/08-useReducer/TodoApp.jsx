f9e7ded40508490163beba2f4e5cd393ed20b685
AlexRodRom <alejandro.rodriguez@hotmail.es>
2023-11-29 15:14:33 +0000

    Add todo Reducer a la tarea

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -3,26 +3,33 @@ import { todoReducer } from "./todoReducer";
 import { TodoList } from "./todoList";
 import { TodoAdd } from "./TodoAdd";
 
+const initialState = [
+    {
+        id: new Date().getTime(),
+        description: 'Recolectar la piedra del Alma',
+        done: false
+    },
+    {
+        id: new Date().getTime() * 3,
+        description: 'Recolectar la piedra del Poder',
+        done: false
+    }
+];
+
 export const TodoApp = () => {
 
-    const initialState = [
-        {
-            id: new Date().getTime(),
-            description: 'Recolectar la piedra del Alma',
-            done: false
-        },
-        {
-            id: new Date().getTime() * 3,
-            description: 'Recolectar la piedra del Poder',
-            done: false
-        }
-    ];
+    
+    const [todos, dispatch ] = useReducer( todoReducer(), initialState )
 
     const handleNewTodo = ( todo ) => {
-        console.log( todo );
-    }
+        const action =  {
+            type: "[TODO] Add Todo",
+            payload: todo
+        } 
 
-    const [todos, dispatch ] = useReducer( todoReducer(), initialState )
+        console.log( todo );
+        dispatch(action);
+    }
 
     return (
 
