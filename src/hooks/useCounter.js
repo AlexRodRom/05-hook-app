f17cb5f6bcc472910f616a722102fe077736bb9b
AlexRodRom <alejandro.rodriguez@hotmail.es>
2023-12-22 11:43:51 +0100

    Add UseFormTesting

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -6,13 +6,13 @@ export const useCounter = ( initialValue = 10  ) => {
     const [ counter , setCounter ] = useState( initialValue );
 
     const suma = ( value = 1 ) => {
-        setCounter(counter + value);
+        setCounter( (current) => current + value);
     }
 
     const resta = ( value = 1 ) => {
         //if(counter === 0 ) return;
 
-        setCounter(counter - value);
+        setCounter( (current) => current - value);
     }
 
     const reset = () => {
--- src/test/hooks/useForm.test.js
+++ src/test/hooks/useForm.test.js
@@ -1,20 +1,68 @@
-import { renderHook } from "@testing-library/react";
+import { fireEvent, renderHook } from "@testing-library/react";
 import { useForm } from "../../hooks/useForm";
-
+import { act } from "react-dom/test-utils";
 
 
 describe('Prubeas en el useForm', () => { 
 
     const initialForm = { 
         username: 'Alejandro',
-            email: 'a@google.com'
+        email: 'a@google.com'
     }
     
     test('debe devolver la informacion por defecto', () => {
         
         const { result } = renderHook( () => useForm(initialForm) );
         const { formState } = result.current;
-        console.log("formState: "+ JSON.stringify(formState));
+        //console.log("formState: "+ JSON.stringify(formState));
         expect(formState).toMatchObject(initialForm);
     });
+
+    test('Debe de cambiar el nombre del formulario', () => {
+        
+        const newValue = 'Juan'
+        const newTarget = {
+                            target :
+                            { 
+                                name :'username',
+                                value : newValue
+                            }
+                        };
+
+        const { result } = renderHook( () => useForm(initialForm) );
+        const { onInputChange } = result.current;
+        act(() => {
+            onInputChange(newTarget);
+        });
+        //console.log("result.current.username: "+ JSON.stringify(result.current.username));
+        //console.log("result.current.formState.username: "+ JSON.stringify(result.current.formState.username));
+
+        expect(result.current.username).toEqual(newValue);
+        expect(result.current.formState.username).toEqual(newValue);
+    });
+
+    test('Debe devolver el valor del formulario por defecto', () => {
+        
+        const newValue = 'Juan'
+        const newTarget = {
+                            target :
+                            { 
+                                name :'username',
+                                value : newValue
+                            }
+                        };
+
+        const { result } = renderHook( () => useForm(initialForm) );
+        const { onInputChange, onResetForm } = result.current;
+        act(() => {
+            onInputChange(newTarget);
+            onResetForm();
+        });
+        console.log("result.current.username: "+ JSON.stringify(result.current.username));
+        console.log("result.current.formState.username: "+ JSON.stringify(result.current.formState.username));
+
+        expect(result.current.username).toEqual(initialForm.username);
+        expect(result.current.formState.username).toEqual(initialForm.username);
+    });
+
  });
